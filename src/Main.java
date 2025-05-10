64494243b8508e90a41e723f1a4e2c167ecaa6c0
Markitchi <mariustawe22@gmail.com>
2025-05-10 22:31:40 +0100

    Commit avec le point d'entree fonctionnel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args){
+import fr.bibliotheque.service.GestionnaireImpl;
 
+public class Main {
+    public static void main(String[] args) {
+        // Initialisation du gestionnaire (connexion à la base de données)
+        GestionnaireImpl gestionnaire = new GestionnaireImpl();
+
+        // Initialisation de la base de données (création des tables si besoin)
+        gestionnaire.initierBaseDeDonnees();
+        System.out.println("Base de données initialisée.");
+
+        // Fermeture de la connexion à la base de données à la fin du programme
+        fr.bibliotheque.util.BaseDeDonnees.getInstance().fermerConnexion();
+        System.out.println("Connexion à la base de données fermée.");
     }
 }
